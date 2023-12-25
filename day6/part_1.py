c3625384b36523086f24bc8c9ff4e15ec37bc99b
Peter Sushko <petersushko@gmail.com>
2023-12-24 23:36:35 -0800

    incorporated feedback

--- day6/part_1.py
+++ day6/part_1.py
@@ -1,13 +1,13 @@
-"""
-For solving problem 6 of advent of code 2023
-"""
+'''
+For solving problem 6 of advent of code 2023.
+'''
 
 import math
 import numpy as np
 
 
-def find_times_dists():
-    """This function returns times and distances as two lists of ints."""
+def parse_file():
+    '''This function returns times and distances as two lists of ints.'''
     with open('data.txt', 'r', encoding='utf-8') as file:
         first_line = file.readline()
         times_list = [int(time) for time in first_line.split() if time.isdigit()]
@@ -16,7 +16,7 @@ def find_times_dists():
         return times_list, distances_list
 
 def how_many_ways_to_win(ex_time, ex_distance):
-    """ This function calculates how many way the race can be won.
+    '''This function calculates how many way the race can be won.
     
     It finds the solution by solving a system of equations:
     x + y = time
@@ -31,21 +31,21 @@ def how_many_ways_to_win(ex_time, ex_distance):
     integers that are between the two solutions. 
 
     Example:
-    >>>how_many_ways_to_win(7,9)
+    >>> how_many_ways_to_win(7,9)
     4
     
     This is because the solutions to x**2 - 7*x + 9 = 0
     are 5.303 and 1.697. There are 4 total integers in this range:
     2, 3, 4 and 5. Thus we can hold the button for this amount of time
     and we will beat the distance.
-    """
+    '''
     sol1, sol2 = np.roots([1,-ex_time,ex_distance])
     upper_bound = math.floor(max(sol1,sol2))
     lower_bound = math.ceil(min(sol1,sol2))
     return upper_bound - lower_bound + 1
 
 ANSWER = 1
-TIMES, DISTANCES = find_times_dists()
+TIMES, DISTANCES = parse_file()
 for race in range(len(TIMES)):
     ANSWER *= how_many_ways_to_win(TIMES[race],DISTANCES[race])
 print(ANSWER)
--- day6/part_2.py
+++ day6/part_2.py
@@ -1,18 +1,18 @@
-"""
-For solving problem 6 part of advent of code 2023
-"""
+'''
+For solving problem 6 part of advent of code 2023.
+'''
 import math
 import numpy as np
 
 def find_times_dists_part2():
-    """This function returns times and distances as ints."""
+    '''This function returns times and distances as ints.'''
     with open('data.txt', 'r', encoding='utf-8') as file:
         concatenated_times = ''.join(filter(str.isdigit, file.readline()))
         concatenated_dists = ''.join(filter(str.isdigit, file.readline()))
         return int(concatenated_times), int(concatenated_dists)
 
 def how_many_ways_to_win(ex_time, ex_distance):
-    """ This function calculates how many way the race can be won.
+    '''This function calculates how many way the race can be won.
     
     It finds the solution by solving a system of equations:
     x + y = time
@@ -27,14 +27,14 @@ def how_many_ways_to_win(ex_time, ex_distance):
     integers that are between the two solutions. 
 
     Example:
-    >>>how_many_ways_to_win(7,9)
+    >>> how_many_ways_to_win(7,9)
     4
     
     This is because the solutions to x**2 - 7*x + 9 = 0
     are 5.303 and 1.697. There are 4 total integers in this range:
     2, 3, 4 and 5. Thus we can hold the button for this amount of time
     and we will beat the distance.
-    """
+    '''
     sol1, sol2 = np.roots([1,-ex_time,ex_distance])
     upper_bound = math.floor(max(sol1,sol2))
     lower_bound = math.ceil(min(sol1,sol2))
