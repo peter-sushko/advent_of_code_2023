f38f1d19d2c8547896dbe5618c138edb8c304e4f
Peter Sushko <petersushko@gmail.com>
2023-12-25 01:12:10 -0800

    cleaned up the solution

--- day2/part_1.py
+++ day2/part_1.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[27]:
-
+'''
+For solving advent of code puzzles day 2.
+'''
 
 def game_is_valid(my_string):
-    """
-    This function checks if the game is valid. If it is valid, it returns its ID. 
-    Otherwise it returns 0
-    It starts finding the game ID by looking between find space and first ":" character
-    Then it loops over the code and finds the indeces of keywords "green", "blue", and "red"
-    When the keyword is struck, it finds the correspoding number
-    The maximum is kept for each color. 
+    '''This function return game ID if the game is valid, 0 otherwise.
+
+    It starts finding the game ID by looking between find space and first ":" character.
+    Then it loops over the code and finds the indeces of keywords "green", "blue", and "red".
+    When the keyword is struck, it finds the correspoding number.
+    The maximum is kept for each color.
     If all colors are less than the given count, the game is valid.
     
     Parameters:
@@ -22,17 +19,17 @@ def game_is_valid(my_string):
     game_number (int): ID of the game if it is valid and 0 othersize
 
     Examples:
-    >>> ex1="Game 1: 20 green, 3 red, 2 blue; 9 red, 16 blue"
+    >>> ex1 = "Game 1: 20 green, 3 red, 2 blue; 9 red, 16 blue"
     >>> game_is_valid(ex1)
-    >>> 0 
+    0 
     because the game is invalid
     
-    >>> ex2="Game 42: 2 green, 3 red, 2 blue; 9 red, 1 blue"
+    >>> ex2 = "Game 42: 2 green, 3 red, 2 blue; 9 red, 1 blue"
     >>> game_is_valid(ex2)
-    >>> 42 
+    42 
     because game ID is 42        
-    """
-    # the line below finds the game number by looking between first space and first ":" symbol
+    '''
+
     game_number = int(my_string[my_string.find(" "):my_string.find(":")])
     greens = blues = reds = 0
     start_index = 0
@@ -61,13 +58,8 @@ def game_is_valid(my_string):
     return 0
 
 
-# In[28]:
-
-
 SOLUTION = 0
-with open('data.txt', 'r') as file:
+with open('data.txt', 'r', encoding = 'utf-8') as file:
     for row in file:
         SOLUTION += game_is_valid(row)
-    print(row)
 print(SOLUTION)
-
--- day2/part_2.py
+++ day2/part_2.py
@@ -1,31 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
-
+'''
+For solving advent of code puzzles day 2.
+'''
 
 def game_is_valid_part2(my_string):
-    """
-    This function returns the product of color counts.
-    Function it loops over the code and finds the indeces of keywords 
-    Keywords are "green", "blue", and "red"
-    When the keyword is struck, it finds the correspoding number
-    The maximum is kept for each color. 
+    """This function returns the product of color counts.
     
     Parameters:
     my_string (str): string of text passed to the function.
-    Example: Game 1: 20 green, 3 red, 2 blue; 9 red, 16 blue
+    Example: Game 1: 20 green, 3 red, 2 blue; 9 red, 16 blue.
 
     Returns:
-    game_number (int): the product of minimum set of cubes to make the game work
+    game_number (int): the product of minimum set of cubes to make the game work.
 
     Examples:
     >>> ex1="Game 1: 20 green, 3 red, 2 blue; 9 red, 16 blue"
     >>> game_is_valid_part2(ex1)
-    >>> 2880
-    because min possible green is 20, blue is 16 and red is 9. Product is 2880
-    
+    2880
+
+    because minimum possible green is 20, blue is 16 and red is 9. Product is 2880.
     """
+
     greens = blues = reds = 0
     start_index = 0
     while start_index < len(my_string):
@@ -50,14 +44,8 @@ def game_is_valid_part2(my_string):
 
     return reds*greens*blues
 
-
-# In[10]:
-
-
 SOLUTION = 0
 with open('data.txt', 'r') as file:
     for row in file:
         SOLUTION += game_is_valid_part2(row)
-    print(row)
 print(SOLUTION)
-
